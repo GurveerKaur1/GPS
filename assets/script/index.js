680c14f24290c2366a1a56013a8f53fb3cd17eff
Gurveer <gurveer2202@gmail.com>
2022-12-13 10:48:47 -0600

    add changes

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,55 +4,59 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZ3VydmVlcjEiLCJhIjoiY2xia3lnanE0MDJibjNycG1hZ
 const round = document.querySelector('.round')
 const message = document.querySelector('.message')
 
-let geolocate = new mapboxgl.GeolocateControl({
-    positionOptions: {
-        enableHighAccuracy: true,
-        watchPosition: true
-    }
-});
-
-
-function getLocation(position) {
-
-    const { altitude, latitude, longitude } = position.coords
-
-
-
-    const map = new mapboxgl.Map({
-        container: 'row',
-
-        style: 'mapbox://styles/mapbox/streets-v12',
-        center: [longitude, latitude],
-        zoom: 16,
-        attributionControl: false,
-    });
-    const marker = new mapboxgl.Marker({
-        color: "red",
-        draggable: true
-    }).setLngLat([longitude, latitude])
-        .addTo(map);
-    map.getUiSettings().setAttributionEnabled(false);
-    map.getUiSettings().setLogoEnabled(false);
-}
-function errorHandler(error) {
-    round.style.display = 'none'
-    message.style.display = 'inline'
-    console.log('Unable to check your location')
-}
-
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(getLocation, errorHandler)
-} else {
-
-
-    console.log('GeoLocation is not supported by this browser')
-}
-
-
-
 function select(selector, parent = document){
     return parent.querySelector(selector)
 }
 
+const map = new mapboxgl.Map({
+    container: 'map',
 
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [0, 0],
+    pitch: 40,
+    zoom: 16,
+   attributionControl: false,
+});
 
+map.dragPan.disable();
+map.keyboard.disable();
+map.scrollZoom.disable();
+map.doubleClickZoom.disable();
+map.touchZoomRotate.disable();
+
+const marker = new mapboxgl.Marker({
+    color:'#3898ff'
+});
+
+function getLocation(position){
+    const{ longitude, latitude} = position.coords;
+
+    if(longitude && latitude) {
+        map.setCenter([longitude, latitude]);
+        marker.setLngLat([longitude, latitude]).addTo(map);
+       // setTimeout(() => {round.style.display = 'none'}, 750)
+    }
+}
+
+function errorHandler(error){
+    round.style.animationPlayState = 'paused'
+   
+    console.log(error.message)
+}
+
+const options = {
+    enableHighAccuracy: true,
+    maximumAge: 0
+};
+
+/*
+The watchPosition() method is used to register a handler function that will
+be called automatically each time the position of the device changes
+*/
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition(getLocation, errorHandler)
+}else{
+
+   
+    console.log('GeoLocation is not supported by this browser')
+}
